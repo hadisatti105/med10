46b44bd3c5cc9e703c0dfc574524999edde6c1d7
hadisatti105 <hadisatti436@gmail.com>
2026-03-16 09:38:07 -0700

    render port fix

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ app.post("/submit-lead", async (req, res) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+ console.log(`Server running on port ${PORT}`)
 })
